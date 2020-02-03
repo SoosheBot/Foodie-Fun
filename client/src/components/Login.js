3caa49214efcb9adf70885613dcb7bc8fbc1cf49
sooshe <sooshe@sooshebot.com>
2020-02-03 14:17:51 -0600

    login form complete

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,1 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const Login = (props) => {
+    const [credentials, setCredentials] = useState({
+        username:'',
+        password:''
+    });
+
+    const handleChange = e => {
+        setCredentials({...credentials, [e.target.name]: e.target.value});
+    };
+
+    const submitForm = e => {
+        e.preventDefault();
+        axiosWithAuth()
+        .post()
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            props.history.push('/protected');
+        })
+        .catch(err => {
+            console.log('login error', err);
+        });
+    };
+
+    return (
+        <div className='login-form'>
+            <form onSubmit={submitForm}>
+                <input type='text'
+                className='username'
+                placeholder='Enter username...'
+                value={credentials.username}
+                onChange={handleChange}
+                />
+                <input type='text'
+                className='password'
+                placeholder='Enter password...'
+                value={credentials.password}
+                onChange={handleChange} 
+                />
+            </form>
+        </div>
+    );
+};
+
+export default Login;
