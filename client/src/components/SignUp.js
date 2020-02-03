9baf95d08d683c5e3bff1e73a1df98c729bdb7c2
sooshe <sooshe@sooshebot.com>
2020-02-03 15:17:14 -0600

    style signup form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,85 +1,85 @@
 import React, { useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+
 import styled from "styled-components";
+import Signup from '../images/Signup.png';
 
 const StyledSignUp = styled.div`
 margin-top: 8%;
   height: 80vh;
   color: #e76e3c;
-
   display: block;
   width: 80%;
   margin: 2rem auto;
 
-  signup-form {	    
+form {
     width: 70%;	    
     margin: 15% auto 2% auto;	    
     padding: 3rem 1rem;	      
     border-radius: 5rem;	     
-    background-color: white;
-    width: 70%;
 
     ::before {
-      background-image: url("../images/SignIn_Page3 hdPTXwI-lc.png");
-
-      background-size: cover;
-      content: "";
-      display: block;
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      z-index: -2;
-      opacity: 0.4;
-    }
-    
-    input[type='text],
-    input[type='number'] {
+        background-image: url(${Signup});
+        background-size: cover;
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -2;
+        opacity: 0.8;
+      }   
+      
+      input[type="text"],
+      input[type="number"] {
         border: 1px solid #e76e3c;
-        width: 70%;
+        border-radius:5px;
+        width: 80%;
         display: block;
         margin: 1rem auto 2rem auto;
         padding: 0.5rem 0;
-    }
+      }
 
-    label {
-      display: block;
-      width: 80%;
-      margin: 2rem auto;
-    }
+      label {
+        display: block;
+        width: 80%;
+        margin: 2rem auto;
+      }
 
-    button {
-      width: 50%;
-      padding: 1rem;
-      background-color: #e76e3c;
-      color: white;
-      border: 1px solid #e76e3c;
-
-      &:hover{
-        background-color: white;
+      button {
+        width: 50%;
+        padding: 1rem;
+        background-color: #e76e3c;
+        border-radius:5px;
+        color: white;
         border: 1px solid #e76e3c;
-        color: #e76e3c;
+  
+        &:hover{
+          background-color: white;
+          border: 1px solid #e76e3c;
+          color: #e76e3c;
+        }
       }
     }
-  }
+    p {
+        color: black;
+        background-color: white;
+        width: 50%;
+        margin: 0 auto;
+        padding: 1rem;
+      }
+    
+      a {
+        color: #e76e3c;
+        text-decoration: none;
+    
+        &: hover {
+          text-decoration: underline;
+        }
+      }
 
-  p {
-    color: black;
-    background-color: white;
-    width: 50%;
-    margin: 0 auto;
-    padding: 1rem;
-  }
-
-  a {
-    color: #e76e3c;
-    text-decoration: none;
-
-    &: hover {
-      text-decoration: underline;
-    }
-  }
 `;
 
 const SignUp = props => {
